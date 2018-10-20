1e0594e4b66ed79a5d4ddfc4efa434ac9dc69f8b
phamhailongg <longphamhai123@gmail.com>
2018-10-20 21:21:43 +0700

    comment

---
+++ session3/CctF.py
@@ -0,0 +1,4 @@
+# Convert Celcius(ºC) into Fahrenheit(ºF)
+C = float(input("Enter the temperature in Celcius? "))
+F = C * 1.8 + 32 
+print(C, "(C)", " = ", F, "(F)")
---
+++ session3/aoc_calc.py
@@ -0,0 +1,4 @@
+# calculate the area of a circle
+r = float(input("radius? "))
+A = r ** 2 * 3.14 
+print("Area = ", A)
